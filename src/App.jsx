2b7f318e8c704e6388df3a77b45cad2cf46a0624
“amanchanda6660” <amanchanda6660@sdsu.edu>
2026-05-20 23:31:29 -0700

    tweaked game card, added logo, made the status centered as well as changed gamecard color too

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import LeagueTabs from './components/LeagueTabs'
 
 function App() {
   const [games, setGames] = useState ([])
-  const [selectedLeague, setLeague] = useState("nfl")
+  const [selectedLeague, setLeague] = useState("nba")
   const leagueUrls ={
     nba: "basketball/nba",
     nfl: "football/nfl",
@@ -44,7 +44,9 @@ return (
       awayTeam={game.competitions[0].competitors[1].team.displayName}
       homeScore={game.competitions[0].competitors[0].score} 
       awayScore={game.competitions[0].competitors[1].score}
-      status={game.status.type.detail} 
+      status={game.status.type.detail}
+      homeImage={game.competitions[0].competitors[0].team.logo}
+      awayImage={game.competitions[0].competitors[1].team.logo}
       />) :<p>No live games right now.</p>
 }   
 
@@ -54,6 +56,8 @@ return (
       homeScore={game.competitions[0].competitors[0].score} 
       awayScore={game.competitions[0].competitors[1].score}
       status={game.status.type.detail}
+      homeImage={game.competitions[0].competitors[0].team.logo}
+      awayImage={game.competitions[0].competitors[1].team.logo}
       />) :<p>No upcoming games right now.</p>
       }
 
@@ -63,6 +67,8 @@ return (
       homeScore={game.competitions[0].competitors[0].score} 
       awayScore={game.competitions[0].competitors[1].score}
       status={game.status.type.detail}
+      homeImage={game.competitions[0].competitors[0].team.logo}
+      awayImage={game.competitions[0].competitors[1].team.logo}
       />):<p>No games have ended.</p>
     } 
 
--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -1,10 +1,24 @@
 import styles from './GameCard.module.css'
 
-function GameCard({homeTeam, awayTeam, homeScore, awayScore, status}){
+function GameCard({homeTeam, awayTeam, homeScore, awayScore, status, homeImage, awayImage}){
 return(
-    <div className = {styles.card}> 
-    <p className={styles.scoreLine}>{awayTeam} {awayScore} @ {homeScore} {homeTeam}</p>
-    <p className={styles.statusText}>{status}</p>
+    <div className = {styles.card}>
+        <div className = {styles.teamSection}>
+             <img className={styles.logos} src={awayImage} alt="Away Team" />
+             <p>{awayTeam}</p>
+             <p>{awayScore}</p>
+        </div>
+        
+        <div className={styles.statusGroup}>
+            <p className={styles.statusText}>@</p>
+            <p className={styles.statusText}>{status}</p>
+        </div>
+        
+        <div className = {styles.teamSection}>
+            <p>{homeScore}</p> 
+            <p>{homeTeam}</p>
+            <img className={styles.logos} src={homeImage} alt="Home Team" />
+        </div>
     </div>
 )
 
