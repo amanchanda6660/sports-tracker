22363a20d01696c51bcf2804dd1e9ce304b7d24e
“amanchanda6660” <amanchanda6660@sdsu.edu>
2026-05-20 17:35:29 -0700

    updated button colors

--- src/components/LeagueTabs.jsx
+++ src/components/LeagueTabs.jsx
@@ -1,11 +1,14 @@
+import styles from './LeagueTabs.module.css'
+
+
 function LeagueTabs({selectedLeague, setLeague}){
 
     return(
         <div>
-        <button onClick={() => setLeague("nba")} >NBA</button>
-        <button onClick={() => setLeague("nfl")} >NFL</button>
-        <button onClick={() => setLeague("mlb")} >MLB</button>
-        <button onClick={() => setLeague("nhl")} >NHL</button>
+        <button className={styles.leagueButton} onClick={() => setLeague("nba")} >NBA</button>
+        <button className={styles.leagueButton} onClick={() => setLeague("nfl")} >NFL</button>
+        <button className={styles.leagueButton} onClick={() => setLeague("mlb")} >MLB</button>
+        <button className={styles.leagueButton} onClick={() => setLeague("nhl")} >NHL</button>
         </div>
         
     )
