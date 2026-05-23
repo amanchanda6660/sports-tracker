99dafffb8b97c2203e4603be1f4f69b2ba65bb52
“amanchanda6660” <amanchanda6660@sdsu.edu>
2026-05-23 12:18:16 -0700

    added logos to league tab buttons

--- src/components/LeagueTabs.jsx
+++ src/components/LeagueTabs.jsx
@@ -5,11 +5,31 @@ function LeagueTabs({selectedLeague, setLeague}){
 
     return(
         <div>
-        <button className={styles.leagueButton} onClick={() => setLeague("nba")} >NBA</button>
-        <button className={styles.leagueButton} onClick={() => setLeague("nfl")} >NFL</button>
-        <button className={styles.leagueButton} onClick={() => setLeague("mlb")} >MLB</button>
-        <button className={styles.leagueButton} onClick={() => setLeague("nhl")} >NHL</button>
-        <button className={styles.leagueButton} onClick={() => setLeague("ucl")} >UCL</button>
+        <button className={styles.leagueButton} onClick={() => setLeague("nba")} >
+            <img src='https://a.espncdn.com/i/teamlogos/leagues/500/nba.png' alt = "NBA" className={styles.leagueLogo}> 
+        </img>
+        NBA
+        </button>
+        <button className={styles.leagueButton} onClick={() => setLeague("nfl")} >
+            <img src='https://a.espncdn.com/i/teamlogos/leagues/500/nfl.png' alt = "NFL" className={styles.leagueLogo}>
+            </img>
+            NFL
+            </button>
+        <button className={styles.leagueButton} onClick={() => setLeague("mlb")} >
+            <img src='https://a.espncdn.com/i/teamlogos/leagues/500/mlb.png' alt = "MLB" className={styles.leagueLogo}>
+           </img>
+            MLB
+            </button>
+        <button className={styles.leagueButton} onClick={() => setLeague("nhl")} >
+            <img src='https://a.espncdn.com/i/teamlogos/leagues/500/nhl.png' alt = "NHL" className={styles.leagueLogo}>
+           </img>
+            NHL
+            </button>
+        <button className={styles.leagueButton} onClick={() => setLeague("ucl")} >
+            <img src='https://a.espncdn.com/i/leaguelogos/soccer/500/2.png' alt = "UCL" className={styles.leagueLogo}>
+           </img>
+            UCL
+            </button>
         </div>
         
     )
